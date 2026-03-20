7d673cc04652ea80a3d8b56b516ad765e1f00770
Formando <b._gabriel@hotmail.com>
2026-03-20 10:35:13 +0000

    homepage

--- src/main/java/pt/upskill/VHS/controllers/HomeController.java
+++ src/main/java/pt/upskill/VHS/controllers/HomeController.java
@@ -6,9 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping(value = "/")
-    public String introPage() {
-        return "intro";
-    }
+
 
 }
