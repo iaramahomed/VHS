d6aaae325669db7e55265c24d0c5d67ca91ec318
iaramahomed <iarams@sapo.pt>
2026-03-23 11:53:22 +0000

    created and added atributes to Rental class

---
+++ src/main/java/pt/upskill/VHS/entities/Rental.java
@@ -0,0 +1,43 @@
+package pt.upskill.VHS.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
+public class Rental {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private LocalDate dateOfRent;
+    private LocalDate dateOfReturn;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDateOfRent() {
+        return dateOfRent;
+    }
+
+    public void setDateOfRent(LocalDate dateOfRent) {
+        this.dateOfRent = dateOfRent;
+    }
+
+    public LocalDate getDateOfReturn() {
+        return dateOfReturn;
+    }
+
+    public void setDateOfReturn(LocalDate dateOfReturn) {
+        this.dateOfReturn = dateOfReturn;
+    }
+}
