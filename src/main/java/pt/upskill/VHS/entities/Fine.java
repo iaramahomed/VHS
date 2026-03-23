32260ad0c935c8fb266dfc1fe4ae91ebb472e6e9
iaramahomed <iarams@sapo.pt>
2026-03-23 11:56:20 +0000

    created and added atributes to Fine class

---
+++ src/main/java/pt/upskill/VHS/entities/Fine.java
@@ -0,0 +1,31 @@
+package pt.upskill.VHS.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Fine {
+
+    @Id
+    @GeneratedValue
+    private long Id;
+
+    private String type;
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long id) {
+        Id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
