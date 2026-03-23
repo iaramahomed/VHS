6d6553d49413fbb86063d8c9b0f5bdff2188e60e
iaramahomed <iarams@sapo.pt>
2026-03-23 12:08:12 +0000

    getters and setters for Reward

--- src/main/java/pt/upskill/VHS/entities/Reward.java
+++ src/main/java/pt/upskill/VHS/entities/Reward.java
@@ -33,4 +33,12 @@ public class Reward {
         this.type = type;
     }
 
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
