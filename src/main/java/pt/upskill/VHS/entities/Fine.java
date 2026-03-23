3c296b23ed81620e14294e2f9c76166724a437e8
iaramahomed <iarams@sapo.pt>
2026-03-23 12:07:58 +0000

    getters and setters for Fine

--- src/main/java/pt/upskill/VHS/entities/Fine.java
+++ src/main/java/pt/upskill/VHS/entities/Fine.java
@@ -32,4 +32,12 @@ public class Fine {
     public void setType(String type) {
         this.type = type;
     }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
